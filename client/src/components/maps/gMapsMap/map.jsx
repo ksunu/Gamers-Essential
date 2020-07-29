dd79169f50809756fe8549453bfa307fdf90286f
cristianvl8 <cristianvinualeslobera@gmail.com>
2020-07-29 19:32:29 +0200

    fixed

--- client/src/components/maps/gMapsMap/map.jsx
+++ client/src/components/maps/gMapsMap/map.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
 import Marker from '../Marker/Marker'
-import mapStyles from '../GmapsStyles'
+
 
 
 export default class GmapMap extends Component {
